7d706668bdbadd3507e3723ba31c81dbff1020b9
JANG-3584765 <wonjun001227@gmail.com>
2026-01-23 02:21:01 +0900

    Update footer.js

--- common/footer.js
+++ common/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../common/footer.html")
+  fetch("/F1/common/footer.html")
     .then(res => res.text())
     .then(html => {
       document.getElementById("footer-container").innerHTML = html;
